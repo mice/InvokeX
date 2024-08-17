d624d705d2cf589121f981256fa1f12f75ea220f
mice.lu <mice_003@163.com>
2024-08-17 20:48:32 +0800

    update

--- Runtime.IL/ILMethod.cs
+++ Runtime.IL/ILMethod.cs
@@ -2,21 +2,28 @@
 using System;
 using System.Reflection;
 
-
-#if !DISABLE_ILRUNTIME
 public class ILMethod : IMethodInfoData
 {
-    public string Name => Data.Name;
-    public string TargetTypeName => Data.DeclearingType.Name;
+    public string Name { get; private set; }
+    public string TargetTypeName { get; private set; }
     public string TypeName => nameof(ILMethod);
     public int ParamCount { get; private set; }
+#if !DISABLE_ILRUNTIME
     public ILRuntime.CLR.Method.IMethod Data;
 
     public ILMethod(ILRuntime.CLR.Method.IMethod method)
     {
         this.Data = method;
+        this.Name = Data.Name;
+        this.TargetTypeName = Data.DeclearingType.Name;
         this.ParamCount = method.ParameterCount;
     }
+#else
+    public ILMethod(object method)
+    {
+       
+    }
+#endif
 
     public ParameterInfo[] GetParameters()
     {
@@ -25,6 +32,7 @@ public class ILMethod : IMethodInfoData
 
     public string ToJson(object[] arr)
     {
+#if !DISABLE_ILRUNTIME
         if (arr.Length > 0)
         {
             SerDict<string, string> data = new SerDict<string, string>();
@@ -37,13 +45,14 @@ public class ILMethod : IMethodInfoData
             var content = JsonUtility.ToJson(data);
             return content;
         }
-
+#endif
         return string.Empty;
     }
 
     public object[] FromJson(string stringObjs)
     {
         object[] objs = new object[0];
+#if !DISABLE_ILRUNTIME
         if (!string.IsNullOrEmpty(stringObjs))
         {
             SerDict<string, string> data = JsonUtility.FromJson<SerDict<string, string>>(stringObjs);
@@ -54,8 +63,7 @@ public class ILMethod : IMethodInfoData
                 objs[i] = data[infos[i].Name] as object;
             }
         }
-
+#endif
         return objs;
     }
 }
-#endif
